28826a030482e7acd420e6ccc782232a71701a2d
kapoorlab <randomaccessiblekapoor@gmail.com>
2018-07-23 18:46:16 +0200

    Upgrading to python 3.6 via command conda create --name 3point6 python=3.6

--- runTensorENV.sh
+++ runTensorENV.sh
@@ -7,8 +7,12 @@ export FIJI_HOME=/data/u934/service_imagerie/v_kapoor/Fiji.app  # Replace with t
 export  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/u934/service_imagerie/v_kapoor/anaconda2/envs/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/u934/service_imagerie/v_kapoor/cuda/lib64/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/u934/service_imagerie/v_kapoor/Fiji.app/lib/linux64/
+
 cd /data/u934/service_imagerie/v_kapoor/anaconda2/bin
 
 #echo ". /data/u934/service_imagerie/v_kapoor/anaconda2/etc/profile.d/conda.sh" >> ~/.bashrc
 #conda activate 
 source activate tensorflow
+conda activate 3point6
+
+
